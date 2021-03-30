7509056d7e9aa18e736c8be0b507372377450e3e
ThomasBarrial <thomas@barrial.fr>
2021-03-30 11:23:39 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const myInformations = require('./information.js')
-const cowsay = require("cowsay");
-
-
-console.log(cowsay.say({
-	text : `Hi my name is ${myInformations.name},from ${myInformations.campus} campus`,
-	e : "oO",
-	T : "U "
-}));
